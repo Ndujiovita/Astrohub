aa7241f1574120cd0653a66235f3db0b0589ac20
Ndujiovita <ndujiovita1@gmail.com>
2024-05-12 13:43:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelector(".view").onclick = 
+function() {
+    document.querySelector(".mybar").classList.toggle("show");
+}
+
+document.querySelector(".remove").onclick = 
+function() {
+    document.querySelector(".mybar").classList.toggle("show");
+}
+
+document.querySelector(".schedule2").onclick =
+function () {
+    const data = document.getElementById("schedule").value;
+console.log("data");
+}
